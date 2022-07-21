6c338120a701d7845057cd865ed4cb0ce1eecb70
manojsingh1999 <95011806+manojsingh1999@users.noreply.github.com>
2022-07-21 11:54:23 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,87 @@
+export const API_GATEWAY_URL = 'https://dev-apigatewaysvc-hwr7nalbaq-el.a.run.app';
+export const API_BASE_URL = API_GATEWAY_URL+'/recruit';
+export const COMPILER_BASE_URL = 'https://codeevaluatorservice-hwr7nalbaq-uc.a.run.app/c1';
+export const ASSESSMENT_SERVICE_URL = API_GATEWAY_URL+'/assessmentservice';
+
+export const REVAL_BASE_URL = API_GATEWAY_URL+'/reval';
+export const PREDICTION='https://dev-careercoach-hwr7nalbaq-el.a.run.app/';
+export const ACCESS_TOKEN = 'accessToken';
+export const OAUTH2_REDIRECT_URI = window.location.origin+'/oauth2/redirect';
+export const API_BASE_UR_MEDIA = 'https://mediauploadservice-hwr7nalbaq-uc.a.run.app/m1';
+export const GOOGLE_AUTH_URL = API_GATEWAY_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const FACEBOOK_AUTH_URL = API_GATEWAY_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const KEY = ((API_GATEWAY_URL === 'http://localhost:9001') ? "DISHOOMREALCODER" : "REALCODERZ123456");
+export const ONLINE_COMPILER_BASE_URL = "https://newcompiler.realcoderz.com";
+export const RESUME_PARSER_URL = 'https://demo-resumeparser-q6uoj7vcrq-uc.a.run.app';
+export const STUDENT_ASSESSMENT_URL =window.location.origin+'/quiz';
+export const url ="https://eventservice-hwr7nalbaq-uc.a.run.app/r1";
+export const IFSC_BASE_URL ='https://ifsc.razorpay.com/';
+export const REDIRECT_URL = window.location.origin+'/oauth2/redirect';
+export const COUNTRY_CODES_AND_DIAL_CODES=[{"name":"India","dial_code":"+91","code":"IN"},{"name":"United States","dial_code":"+1","code":"US"},{"name":"Afghanistan","dial_code":"+93","code":$
+
+export const GRADUATION_NAME = [{"name":"Associate of Arts"},{"name":"Associate of Applied Arts"},{"name":"Associate of Engineering or Associate in Electronics Engineering Technology"},{"name$
+export const MAIL_SERVICE = 'https://dev-emailservice-hwr7nalbaq-el.a.run.app/api';
+
+export const ONBOARD_BASE_URL = "https://dev-apigatewaysvc-hwr7nalbaq-el.a.run.app/onboarding";
+export const SKILLBUILDER_BASE_URL ='https://dev-apigatewaysvc-hwr7nalbaq-el.a.run.app/skillbuilder/';
+export const PAYROLL_BASE_URL = "https://dev-payrollbackendsvcc-hwr7nalbaq-el.a.run.app";
+export const PERFORMANCE_API_URL = "https://dev-performancestag-hwr7nalbaq-el.a.run.app";
+export const EMAIL_URL = API_GATEWAY_URL+"/skillbuilder";
+export const PAYMENT_BASE_URL = 'https://rcreston.com/payment-gateway/api';
+export const COMMONSURVEY_BASE_URL = 'https://dev-survey-backend-hwr7nalbaq-el.a.run.app/       ';
+export const MAIL_REPORT_SERVICE = "https://api.smtp2go.com/v3";
+export const MAIL_REPORT_API_KEY = "api-BC51BE0CD50911ECACBFF23C91C88F4E";
+
+export const SkillMatchBaseUrl = 'https://dev-careercoach-hwr7nalbaq-el.a.run.app';
+export const TIMESHEET_BASE_URL='https://dev-apigatewaysvc-hwr7nalbaq-el.a.run.app/timesheet';
+export const users="/users";
+export const timeSheet="/timesheet";
+export const task="/task";
+export const reimb="/reimbursement";
+export const project="/project";
+export const depart="/department";
+export const attend="/attendance";
+export const event = "/event";
+export const leave = "/leave";
+export const ticket="/ticket";
+export const holiday="/holiday";
+export const ticket_type ='/type';
+export const employeetype="/employeetype";
+export const leavePolicy="/leavepolicy";
+export const leavepolicymapping="/leavepolicymapping";
+export const WEBSOCKET_URL= "https://dev-apigatewaysvc-hwr7nalbaq-el.a.run.app/skillbuilder/";
+export const group="/group";
+export const groupmapping="/groupmapping";
+export const employeeleavedetails="/employeeleavedetails";
+export const leaveLeaveApproval='/leaveLeaveApproval';
+export const apisec_org_id = 2;
+export const shift = "/shift" 
+export const trialOrgName = "360degreecoder";
+export const trialOrgLink = "https://360degreecoder.com";
+export const HFS_ORG_ID = 3;
+export const COMMON_ORG_NAME = "dev-rcteambuilderui-hwr7nalbaq-uc";
+export const GENCONNECT_ORG_ID = 3;
+export const workType = "/worktype";
+export const resign ="/resign";
+export const bulkHolidayTemplatePath='https://storage.googleapis.com/rcpublicimages/Holiday/HolidayTemplate.xlsx';
+export const bulkTemplatePath = "https://storage.googleapis.com/rcpublicimages/Bulk_Employee.xlsx";
+export const publicEmailDomains =["gmail.com","yahoo.com", "hotmail.com","aol.com", "hotmail.co.uk", "hotmail.fr","msn.com"
+
+,"yahoo.fr","wanadoo.fr","orange.fr","comcast.net", "yahoo.co.uk","yahoo.com.br","yahoo.co.in","live.com","rediffmail.com","free.fr",
+
+"gmx.de","web.de", "yandex.ru", "ymail.com", "libero.it", "outlook.com", "uol.com.br", "bol.com.br", "mail.ru", "cox.net","hotmail.it",
+
+"sbcglobal.net", "sfr.fr" ,"live.fr", "verizon.net", "live.co.uk", "googlemail.com", "yahoo.es", "ig.com.br", "live.nl", "bigpond.com", "terra.com.br",
+
+"yahoo.it", "neuf.fr", "yahoo.de", "alice.it", "rocketmail.com", "att.net", "laposte.net", "facebook.com", "bellsouth.net", "yahoo.in",
+"hotmail.es", "charter.net", "yahoo.ca", "yahoo.com.au", "rambler.ru", "hotmail.de", "tiscali.it", "shaw.ca", "yahoo.co.jp", "sky.com",
+
+"earthlink.net", "optonline.net", "freenet.de", "t-online.de", "aliceadsl.fr", "virgilio.it", "home.nl", "qq.com", "telenet.be", "me.com", "yahoo.com.ar",
+
+"tiscali.co.uk", "yahoo.com.mx", "voila.fr", "gmx.net", "mail.com", "planet.nl", "tin.it", "live.it", "ntlworld.com", "arcor.de", "yahoo.co.id", "frontiernet.net",
+
+"hetnet.nl", "live.com.au", "yahoo.com.sg", "zonnet.nl", "club-internet.fr", "juno.com", "optusnet.com.au", "blueyonder.co.uk","bluewin.ch", "skynet.be",
+
+"sympatico.ca", "windstream.net", "mac.com", "centurytel.net", "chello.nl", "live.ca", "aim.com", "bigpond.net.au"
+
+]
